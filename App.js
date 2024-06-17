6386394b571f9a83c03d1ef77d53fed393d17b74
Benjamin Pmt <benjamin.pimont@laposte.net>
2024-06-17 16:50:21 +0200

    add images

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Premier Commit</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
